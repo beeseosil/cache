4f367e45b94b4a6b240f104b8287b81410c3d22a
yuninze-dto <gajeongsick@gmail.com>
2025-01-21 17:42:21 +0900

    #

---
+++ _template.py
@@ -0,0 +1,25 @@
+
+# Scheduler, Webserver, MetadataDb, Executor, Worker (of DAGs)
+## LocalExecutor, CeleryExecutor, KubernatesExcutor
+## Tasks are stored as script file in designated directories
+
+# Airflow
+
+with Task(
+    'taskname',
+    default_args={},
+    schedule='on_demand',
+    start_date=pendulum.datetime(2025,1,1,tz='UTC')
+) as task:
+    def extract(**kwargs):
+        pass
+    
+    def transform_q(**kwargs):
+        pass
+
+    def transform_w(**kwargs):
+        pass
+
+    extract_task=PythonOperater(task_id,Callable)
+    transform_q_task=PythonOperater(task_id,Callable)
+    transform_w_task=PythonOperater(task_id,Callable)
